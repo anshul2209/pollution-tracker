14b885d4b15d3ff76c23f13a638ae20f1d9e5da1
Anshul <anshul.bansal@olx.com>
2020-03-24 23:23:33 +0530

    Added pkgjson config

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import svelte from 'rollup-plugin-svelte';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import replace from '@rollup/plugin-replace';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 
